a2a3c02c63b39b3a74d60e7964ed126abeed141e
AsiiaPine <asiiapine@gmail.com>
2025-05-13 21:38:02 +0300

    write eeprom until match

--- include/storage.h
+++ include/storage.h
@@ -2,14 +2,6 @@
 
 #include <EEPROM.h>
 
-inline void save_uint16(uint16_t value, uint16_t address) {
-  EEPROM.put(address, value);
-}
-
-inline void save_uint32(uint32_t value, uint16_t address) {
-  EEPROM.put(address, value);
-}
-
 uint16_t load_uint16(uint16_t address) {
   uint16_t value;
   EEPROM.get(address, value);
@@ -20,4 +12,16 @@ uint32_t load_uint32(uint16_t address) {
   uint32_t value;
   EEPROM.get(address, value);
   return value;
+}
+
+inline void save_uint16(uint16_t value, uint16_t address) {
+  while (load_uint16(address) != value) {
+    EEPROM.put(address, value);
+  }
+}
+
+inline void save_uint32(uint32_t value, uint16_t address) {
+  while (load_uint32(address) != value) {
+    EEPROM.put(address, value);
+  }
 }
